1a00ff9bf2f278f2ebc226b11ee94caf7abda340
Jesse Allen <j6allen@gmail.com>
2017-04-25 19:38:15 -0700

    Initial

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidBody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+
+    }
+
+    private void Move(float h, float v)
+    {
+        movement.Set(h, 0.0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+   
+
+}
