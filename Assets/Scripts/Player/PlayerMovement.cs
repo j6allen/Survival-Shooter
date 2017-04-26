9e0ece4867a24729f814610327152339507efd4c
Jesse Allen <j6allen@gmail.com>
2017-04-25 21:10:08 -0700

    Added player movement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,36 +2,57 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 6f;
-
-    Vector3 movement;
-    Animator anim;
-    Rigidbody playerRigidBody;
-    int floorMask;
-    float camRayLength = 100f;
+    public float speed = 6;
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody playerRidgedBody;
+    private int floorMask;
+    private float camRayLength = 100f;
 
     private void Awake()
     {
         floorMask = LayerMask.GetMask("Floor");
         anim = GetComponent<Animator>();
-        playerRigidBody = GetComponent<Rigidbody>();
+        playerRidgedBody = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        Move(h, v);
 
+        
+        Move(h, v);
+        Turning();
+        Animating(h,v);
     }
 
     private void Move(float h, float v)
     {
-        movement.Set(h, 0.0f, v);
+        movement.Set(h, 0f, v);
         movement = movement.normalized * speed * Time.deltaTime;
 
-        playerRigidBody.MovePosition(transform.position + movement);
+        playerRidgedBody.MovePosition(transform.position + movement);
     }
-   
 
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRidgedBody.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
 }
